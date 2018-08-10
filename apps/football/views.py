0ec4857a26fbcaaf5a9ecbd193fa160f258280b4
Max <mjann@ucsc.edu>
2018-08-10 12:58:35 -0700

    fantasy football madness

---
+++ apps/football/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from models import *
+
+# Create your views here.
+def index(request):
+    context = {
+        "my_players": Player.objects.filter(owner = User.objects.get(id=2))
+    }
+    return render(request, 'football/index.html', context)
+def players(request):
+    context = {
+        "players" : Player.objects.all()
+    }
+    return render(request, 'football/players.html', context)
