00159ebf2ed7bc8f2a121cf6057922be483cdd6c
Max <mjann@ucsc.edu>
2018-08-11 13:42:02 -0700

    added some real data to index

--- apps/football/views.py
+++ apps/football/views.py
@@ -7,7 +7,14 @@ from models import *
 # Create your views here.
 def index(request):
     context = {
-        "my_players": Player.objects.filter(owner = User.objects.get(id=2))
+        "my_players": Player.objects.filter(owner = User.objects.get(id=2)),
+        "my_rbs": Player.objects.filter(owner = User.objects.get(id=2)).filter(position = "running back"),
+        "my_wrs": Player.objects.filter(owner = User.objects.get(id=2)).filter(position = "wide receiver"),
+        "my_qbs": Player.objects.filter(owner = User.objects.get(id=2)).filter(position = "quarterback"),
+        "my_tes": Player.objects.filter(owner = User.objects.get(id=2)).filter(position = "tight end"),
+
+
+
     }
     return render(request, 'football/index.html', context)
 def players(request):
