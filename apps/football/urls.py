93d93e6a2fe96cd7ccda23ec44248f3968277f1b
Max <mjann@ucsc.edu>
2018-08-12 03:09:53 -0700

    finishing touches

--- apps/football/urls.py
+++ apps/football/urls.py
@@ -3,9 +3,18 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^team$', views.team),
-
-
+    # url(r'^team$', views.team),
     url(r'^players$', views.players),
+    url(r'^team/(?P<user_id>\d+)$', views.teamx),
+    url(r'^qbs$', views.qbs),
+    url(r'^rbs$', views.rbs),
+    url(r'^wrs$', views.wrs),
+    url(r'^tes$', views.tes),
+    url(r'^player/(?P<player_id>\d+)$', views.playerx),
+
+
+    url(r'^', views.odell),
+
+
 
 ]
--- apps/football/views.py
+++ apps/football/views.py
@@ -7,11 +7,7 @@ from models import *
 # Create your views here.
 def index(request):
     context = {
-        "my_players": Player.objects.filter(owner = User.objects.get(id=2)),
-        "my_rbs": Player.objects.filter(owner = User.objects.get(id=2)).filter(position = "running back"),
-        "my_wrs": Player.objects.filter(owner = User.objects.get(id=2)).filter(position = "wide receiver"),
-        "my_qbs": Player.objects.filter(owner = User.objects.get(id=2)).filter(position = "quarterback"),
-        "my_tes": Player.objects.filter(owner = User.objects.get(id=2)).filter(position = "tight end"),
+        "users": User.objects.exclude(id=1)
     }
     return render(request, 'football/index.html', context)
 def team(request):
@@ -29,3 +25,44 @@ def players(request):
         "users" : User.objects.all()
     }
     return render(request, 'football/players.html', context)
+def teamx(request, user_id):
+    print user_id
+    context = {
+        # switch context to be by id
+        "user": User.objects.get(id=user_id),
+        "my_players": Player.objects.filter(owner = User.objects.get(id=user_id)),
+        "my_rbs": Player.objects.filter(owner = User.objects.get(id=user_id)).filter(position = "running back"),
+        "my_wrs": Player.objects.filter(owner = User.objects.get(id=user_id)).filter(position = "wide receiver"),
+        "my_qbs": Player.objects.filter(owner = User.objects.get(id=user_id)).filter(position = "quarterback"),
+        "my_tes": Player.objects.filter(owner = User.objects.get(id=user_id)).filter(position = "tight end"),
+    }
+    return render(request, 'football/teamx.html', context)
+def qbs(request):
+    context = {
+        "qbs": Player.objects.filter(position = "quarterback"),
+    }
+    return render(request, 'football/qbs.html', context)
+def rbs(request):
+    context = {
+        "rbs": Player.objects.filter(position = "running back"),
+    }
+    return render(request, 'football/rbs.html', context)
+def wrs(request):
+    context = {
+        "wrs": Player.objects.filter(position = "wide receiver"),
+    }
+    return render(request, 'football/wrs.html', context)
+def tes(request):
+    context = {
+        "tes": Player.objects.filter(position = "tight end"),
+    }
+    return render(request, 'football/tes.html', context)
+def playerx(request, player_id):
+    print player_id
+    context = {
+        "player": Player.objects.get(id=player_id),
+    }
+    return render(request, 'football/player.html', context)
+
+def odell(request):
+    return render(request, 'football/odell.html')
